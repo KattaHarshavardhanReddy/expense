9cb73c7032ace9656f21aa1c2636c43ce2406a20
KattaHarshavardhanReddy <katta.harshareddy@gmail.com>
2025-06-25 00:35:25 +0530

    db

--- backend.sh
+++ backend.sh
@@ -68,6 +68,7 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 BACKEND $? "Download the application code"
 
 cd /app
+rm -rf /app/*
 
 unzip /tmp/backend.zip &>>$Log_Name
 BACKEND $? "Unzipping the file to app directory"
