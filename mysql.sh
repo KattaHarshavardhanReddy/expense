2c821d8545a86175dc27e68e18147a8df27e3e1a
KattaHarshavardhanReddy <katta.harshareddy@gmail.com>
2025-06-24 11:40:01 +0530

    db

--- mysql.sh
+++ mysql.sh
@@ -5,6 +5,13 @@ G="\e[32m"
 Y="\e[34m"
 N="\e[0m"
 
+logs_folder="/var/log/expense-logs/"
+logs_files="$(echo $0 | cut -d "." -f1)"
+Timestamp=$(date +%y-%m-%d-%H-%M-%S)
+Log_Name="$logs_folder/$logs_files-$Timestamp.log"
+
+echo " Script started at: $Timestamp " &>>$Log_Name
+
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
@@ -27,7 +34,7 @@ if [ $? -ne 0 ]
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    dnf install mysqld -y
+    dnf install mysql -y
     MYSQLINSTALL $? "installing mysql"
 else
     echo -e " $Y MYsql is already installed $N"
